be24ee69653d3454a356e5f71824d6831d81d48e
school <school@A45PC14>
2025-03-11 09:51:28 +0200

    add a classes

--- Program.cs
+++ Program.cs
@@ -2,12 +2,32 @@
 
 
 16. *** Словник ***
-Створити програму, яку буде зберігати словники різними мовами і дозволяти користувачеві знаходити переклад потрібного слова або фрази.
-	Створення, редагування, видалення словників
-	Додавання, редагування, видалення слів в словнику та їх перекладів
-	Пошук слів та перекладів
-	Зберігати словники в файл
+// - Створити програму, яку буде зберігати словники різними мовами 
+// - дозволяти користувачеві знаходити переклад потрібного слова або фрази.
+// - Створення, редагування, видалення словників
+// - Додавання, редагування, видалення слів в словнику та їх перекладів
+// - Пошук слів та перекладів
+// - Зберігати словники в файл
 
 Console.WriteLine("Hello, Dictionary!");
 Console.WriteLine("-------- menu: ---------");
 Console.WriteLine("1. Save Vocabulary");
+
+
+        Console.WriteLine("\tMENU:");
+Console.WriteLine("\t1. Add new translate ");
+Console.WriteLine("\t2. Save to file");
+Console.WriteLine("\t3. Load from file");
+Console.WriteLine("\t4. Show all translates");
+Console.WriteLine("\t5. Delete translate");
+
+
+public class Dictionary
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int Quantity { get; set; }
+
+}
+
+class Translate
