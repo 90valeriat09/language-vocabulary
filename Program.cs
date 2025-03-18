f5a4df11070e8eac49727e723fd97913c36fb649
school <school@A45PC14>
2025-03-18 10:00:46 +0200

    add classes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-16. *** Словник ***
-// - Створити програму, яку буде зберігати словники різними мовами 
+﻿// 16. *** Словник ***
+// - Створити програму, яка буде зберігати словники різними мовами 
 // - дозволяти користувачеві знаходити переклад потрібного слова або фрази.
 // - Створення, редагування, видалення словників
 // - Додавання, редагування, видалення слів в словнику та їх перекладів
@@ -14,12 +11,48 @@ Console.WriteLine("-------- menu: ---------");
 Console.WriteLine("1. Save Vocabulary");
 
 
-        Console.WriteLine("\tMENU:");
+Console.WriteLine("\tMENU:");
 Console.WriteLine("\t1. Add new translate ");
 Console.WriteLine("\t2. Save to file");
 Console.WriteLine("\t3. Load from file");
 Console.WriteLine("\t4. Show all translates");
 Console.WriteLine("\t5. Delete translate");
+
+// Dictionary language = new();
+
+while (true)
+
+{
+
+    Console.Write("Your choice: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    {
+        Console.Write("Your choice: ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+     {
+
+            Console.Write("Enter dictionary name: ");
+            dictionary.Name = Console.ReadLine();
+            Console.Write("Enter category: ");
+            dictionary.Category = Console.ReadLine();
+            Console.Write("Enter category price: ");
+            dictionary.Price = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter dictionary quantity: ");
+
+
+            Console.WriteLine("------- Product ---------");
+            Console.WriteLine($"Name: {product.Name}");
+            Console.WriteLine($"Category: {product.Category}");
+            Console.WriteLine($"Price: {product.Price}");
+            Console.WriteLine($"Quantity: {product.Quantity}");
+            Console.WriteLine($"Manufacturer: {product.Manufacturer}");
+            break;
+        }
+    }
 
 
 public class Dictionary
@@ -30,4 +63,4 @@ public class Dictionary
 
 }
 
-class Translate
+
