37cac491ca33bbc564a4b578bf0919a4abb75a1b
school <school@A45PC14>
2025-04-22 09:10:44 +0300

    gbytg

--- Program.cs
+++ Program.cs
@@ -10,49 +10,54 @@ Console.WriteLine("Hello, Dictionary!");
 Console.WriteLine("-------- menu: ---------");
 Console.WriteLine("1. Save Vocabulary");
 
-
 Console.WriteLine("\tMENU:");
+Console.WriteLine("\t0. Exit");
 Console.WriteLine("\t1. Add new translate ");
 Console.WriteLine("\t2. Save to file");
 Console.WriteLine("\t3. Load from file");
 Console.WriteLine("\t4. Show all translates");
 Console.WriteLine("\t5. Delete translate");
 
-// Dictionary language = new();
+Dictionary language = new();
 
 while (true)
-
 {
-
     Console.Write("Your choice: ");
     int choice = Convert.ToInt32(Console.ReadLine());
 
+    switch (choice)
     {
-        Console.Write("Your choice: ");
-        int choice = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter dictionary name: ");
+    newItem.Name = Console.ReadLine();
+    Console.Write("Enter dictionary category: ");
+    newItem.Category = Console.ReadLine();
+    Console.Write("Enter dictionary price: ");
+    newItem.Price = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter dictionary quantity: ");
+    newItem.Quantity = Convert.ToInt32(Console.ReadLine());
+    
 
-        switch (choice)
-        {
-     {
+        case 1:
+    Console.Write("Enter dictionary name: ");
+    newItem.Name = Console.ReadLine();
+    Console.Write("Enter dictionary category: ");
+    newItem.Category = Console.ReadLine();
+    Console.Write("Enter dictionary price: ");
+    newItem.Price = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter dictionary quantity: ");
+    newItem.Quantity = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter dictionary name: ");
-            dictionary.Name = Console.ReadLine();
-            Console.Write("Enter category: ");
-            dictionary.Category = Console.ReadLine();
-            Console.Write("Enter category price: ");
-            dictionary.Price = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter dictionary quantity: ");
-
-
-            Console.WriteLine("------- Product ---------");
-            Console.WriteLine($"Name: {product.Name}");
-            Console.WriteLine($"Category: {product.Category}");
-            Console.WriteLine($"Price: {product.Price}");
-            Console.WriteLine($"Quantity: {product.Quantity}");
-            Console.WriteLine($"Manufacturer: {product.Manufacturer}");
-            break;
-        }
+    case 4:
+    Console.WriteLine("------- Product ---------");
+    Console.WriteLine($"Name: {product.Name}");
+    Console.WriteLine($"Category: {product.Category}");
+    Console.WriteLine($"Price: {product.Price}");
+    Console.WriteLine($"Quantity: {product.Quantity}");
+    Console.WriteLine($"Manufacturer: {product.Manufacturer}");
+           
+    break;
     }
+}
 
 
 public class Dictionary
